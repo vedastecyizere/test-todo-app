785710103769ce6ec8c9448fdca89810d3d90198
Vedaste Cyizere <revedaste@gmail.com>
2017-03-07 10:31:48 +0200

    tested the deadline, decrement and increment priority

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -14,4 +14,43 @@ RSpec.describe Task, type: :model do
       expect(task.complete).to eq(true)
     end
   end
+
+  describe '#toggle_favorite!' do 
+    it 'should switch favorite to true if it began as false' do
+      task = Task.new(favorite: false)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(true)
+    end 
+  end 
+
+  describe '#toggle_favorite!' do
+    it 'should switch favorite to false if it began as true' do 
+      task = Task.new(favorite: true)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(false)
+    end 
+  end
+
+  describe '#overdue?' do
+    it 'should show prove that current time is greater than the deadline' do
+      task = Task.new(deadline: Time.now - 5.hours)
+      expect(task.overdue?).to eq(true)
+    end 
+  end 
+
+  describe '#increment_priority!' do 
+    it 'should return priority + 1 if priority is less than 10' do 
+      task = Task.new(priority: 8)
+      task.increment_priority!
+      expect(task.priority).to eq(9)
+    end 
+  end
+
+  describe '#decrement_priority!' do 
+    it 'should return the priority - 1 if priority is greater than 1' do 
+      task = Task.new(priority: 4)
+      task.decrement_priority!
+      expect(task.priority).to eq(3)
+    end 
+  end  
 end
